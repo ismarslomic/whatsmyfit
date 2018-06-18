4e6b46ec9fcc4cf08204b2a5c069078a42a15feb
ismslo <ismar.slomic@vegvesen.no>
2018-06-18 13:58:58 +0200

    #13 removed params for environment and status for sslredirect and added test

--- server/common/sslRedirect.ts
+++ server/common/sslRedirect.ts
@@ -4,13 +4,13 @@
  * Force load with https on production environment
  * https://devcenter.heroku.com/articles/http-routing#heroku-headers
  */
-export default function (environments?, status?) {
-  const env = environments || ['production'];
-  const httpStatus = status || 302;
+export default function () {
+  const envs = ['staging', 'production'];
+  const redirectStatus = 302;
   return function (req, res, next) {
-    if (env.indexOf(process.env.NODE_ENV) >= 0) {
+    if (envs.indexOf(process.env.NODE_ENV) >= 0) {
       if (req.headers['x-forwarded-proto'] !== 'https') {
-        res.redirect(httpStatus, 'https://' + req.hostname + req.originalUrl);
+        res.redirect(redirectStatus, 'https://' + req.hostname + req.originalUrl);
       }
       else {
         next();
